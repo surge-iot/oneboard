ea49b19170264f0657da4ffc70b49331c86eb0c0
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-14 07:34:15 +0530

    feat: Adds API integration to class-builder

--- src/app/class-builder/components/base/base.component.ts
+++ src/app/class-builder/components/base/base.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { LocationClassService } from '../../services/location-class.service';
+
+export interface Node{
+  id: string;
+  label: string;
+}
+
+export interface Link{
+  id: string;
+  source: string;
+  target: string;
+}
 
 @Component({
   selector: 'app-base',
@@ -16,6 +29,38 @@ export class BaseComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private route: ActivatedRoute, private router: Router, private locationClassService: LocationClassService) {
+  }
+  classType:string;
+  nodes: Node[];
+  links: Link[];
 
+  ngOnInit() {
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        of(params.get('class-type'))
+      )
+    ).subscribe((d) => {
+      this.classType = d;
+      this.postInit();
+    });
+
+  }
+
+  async postInit() {
+    const classes = await this.locationClassService.findAll().toPromise();
+    this.nodes = classes.map(c =>{
+      return {
+        id:c.id,
+        label: c.name
+      }
+    });
+    this.links = classes.filter(c=>c.parentId!==null).map(c =>{
+      return {
+        id:`${c.id}-${c.parentId}`,
+        target: c.id,
+        source: c.parentId
+      }
+    });
+  }
 }
