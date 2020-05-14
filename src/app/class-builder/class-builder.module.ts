f6d70551ab081281c95e2924f99a0130f5611a9e
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-14 06:47:10 +0530

    feat: Adds ngx-graph

--- src/app/class-builder/class-builder.module.ts
+++ src/app/class-builder/class-builder.module.ts
@@ -9,7 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BaseComponent } from './components/base/base.component';
-
+import {NgxGraphModule} from '@swimlane/ngx-graph';
 
 @NgModule({
   declarations: [BaseComponent],
@@ -21,7 +21,8 @@ import { BaseComponent } from './components/base/base.component';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    NgxGraphModule
   ]
 })
 export class ClassBuilderModule { }
