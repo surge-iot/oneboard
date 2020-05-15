ed7ddd642df8151c604cb9be43045d82811caeb9
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-15 06:19:20 +0530

    feat: Adds location service to hierarchy component
    Updates location interface

--- src/app/architect/components/hierarchy/hierarchy.component.ts
+++ src/app/architect/components/hierarchy/hierarchy.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Node, Link } from '../../../utils/interfaces/graph.interface';
+import { LocationService } from '../../../location/location.service';
 
 @Component({
   selector: 'app-hierarchy',
@@ -8,10 +9,29 @@ import { Node, Link } from '../../../utils/interfaces/graph.interface';
 })
 export class HierarchyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private locationService: LocationService) { }
+
   nodes:Node[];
   links:Link[];
+
   ngOnInit(): void {
+    this.postInit();
   }
 
+  async postInit() {
+    const locations = await this.locationService.findAll().toPromise();
+    this.nodes = locations.map(c => {
+      return {
+        id: 'location-'+c.id,
+        label: c.name
+      }
+    });
+    this.links = locations.filter(c => c.parentId !== null).map(c => {
+      return {
+        id: `link-${c.id}-${c.parentId}`,
+        target: 'location-'+c.id,
+        source: 'location-'+c.parentId
+      }
+    });
+  }
 }
--- src/app/location/location.service.ts
+++ src/app/location/location.service.ts
@@ -9,41 +9,47 @@ import { catchError, retry } from 'rxjs/operators';
 export interface Location {
   id: number;
   name: string;
-  network: string;
-  properties: object;
-  isLocatedIn:number;
-  children:Array<Location>
+  classId: string;
+  parentId: number | null;
+  meta: {};
+  children: Location[]
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-  locationUrl:string;
+  locationUrl: string;
   constructor(private http: HttpClient, private responseService: ResponseService) {
     this.locationUrl = environment.apiRoot + 'location/';
   }
   // Get all root-level locations that the logged-in user is allowed to access
   getRootLocations(): Observable<Location[]> {
+    return this.http.get<Location[]>(this.locationUrl + '?parentId=null').pipe(
+      catchError(this.responseService.handleError)
+    );
+  }
+  // Get all locations that the logged-in user is allowed to access
+  findAll(): Observable<Location[]> {
     return this.http.get<Location[]>(this.locationUrl).pipe(
       catchError(this.responseService.handleError)
     );
   }
   // Get details abouut location identified by id
-  getLocation (id:number): Observable<Location> {
-    return this.http.get<Location>(this.locationUrl+id).pipe(
+  getLocation(id: number): Observable<Location> {
+    return this.http.get<Location>(this.locationUrl + id).pipe(
       catchError(this.responseService.handleError)
     );
   }
   // Get children of location
-  getChildren (id:number): Observable<Location[]> {
-    return this.http.get<Location[]>(this.locationUrl+'?parentId='+id).pipe(
+  getChildren(id: number): Observable<Location[]> {
+    return this.http.get<Location[]>(this.locationUrl + '?parentId=' + id).pipe(
       catchError(this.responseService.handleError)
     );
   }
   // Get path of location
-  getPath (id:number): Observable<Location[]> {
-    return this.http.get<Location[]>(this.locationUrl+id+'/path').pipe(
+  getPath(id: number): Observable<Location[]> {
+    return this.http.get<Location[]>(this.locationUrl + id + '/path').pipe(
       catchError(this.responseService.handleError)
     );
   }
