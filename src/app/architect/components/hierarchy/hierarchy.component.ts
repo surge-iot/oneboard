8e6ce7f98cdf93ae31f30851fe9d2b96d2c1bd83
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-15 07:43:56 +0530

    feat: Adds equipment service to hierarchy component

--- src/app/architect/components/hierarchy/hierarchy.component.ts
+++ src/app/architect/components/hierarchy/hierarchy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Node, Link } from '../../../utils/interfaces/graph.interface';
 import { LocationService } from '../../../location/location.service';
+import { EquipmentService } from '../../../equipment/equipment.service';
 
 @Component({
   selector: 'app-hierarchy',
@@ -9,10 +10,11 @@ import { LocationService } from '../../../location/location.service';
 })
 export class HierarchyComponent implements OnInit {
 
-  constructor(private locationService: LocationService) { }
+  constructor(private locationService: LocationService,
+    private equipmentService: EquipmentService) { }
 
-  nodes:Node[];
-  links:Link[];
+  nodes: Node[];
+  links: Link[];
 
   ngOnInit(): void {
     this.postInit();
@@ -20,34 +22,74 @@ export class HierarchyComponent implements OnInit {
 
   async postInit() {
     const locations = await this.locationService.findAll().toPromise();
+    const equipments = await this.equipmentService.findAll().toPromise();
     this.nodes = locations.map(c => {
       return {
-        id: 'location-'+c.id,
+        id: 'location-' + c.id,
         label: c.name
       }
     });
+    this.nodes = [...this.nodes,
+      ...equipments.map(c => {
+      return {
+        id: 'equipment-' + c.id,
+        label: c.name
+      }
+    })]
 
     let relationships = [];
-    for(let l of locations){
-      relationships.push(...l.children.map(c =>{
+    for (let l of locations) {
+      relationships.push(...l.children.map(c => {
         return {
-          id: `link-${c.id}-${l.id}`,
-          target: 'location-'+c.id,
-          source: 'location-'+l.id,
+          id: `link-l${c.id}-l${l.id}`,
+          target: 'location-' + c.id,
+          source: 'location-' + l.id,
           stroke: '#009688'
         }
       }));
 
-      relationships.push(...l.links.map(c =>{
+      relationships.push(...l.links.map(c => {
         return {
-          id: `link-${c.id}-${l.id}`,
-          target: 'location-'+c.id,
-          source: 'location-'+l.id,
-          stroke: '#2196f3',
-          strokeDashArray:"10, 10"
+          id: `link-l${c.id}-l${l.id}`,
+          target: 'location-' + c.id,
+          source: 'location-' + l.id,
+          stroke: '#009688',
+          strokeDashArray: "10, 10"
         }
       }));
     };
+
+    for (let e of equipments) {
+      // Equipment children
+      relationships.push(...e.children.map(c => {
+        return {
+          id: `link-e${c.id}-e${e.id}`,
+          target: 'equipment-' + c.id,
+          source: 'equipment-' + e.id,
+          stroke: '#f43336'
+        }
+      }));
+      // Equipment links
+      relationships.push(...e.links.map(c => {
+        return {
+          id: `link-e${c.id}-e${e.id}`,
+          target: 'equipment-' + c.id,
+          source: 'equipment-' + e.id,
+          stroke: '#f43336',
+          strokeDashArray: "10, 10"
+        }
+      }));
+
+    };
+    // Equipment locations
+    relationships.push(...equipments.filter(e=>e.locationId!==null).map(e => {
+      return {
+        id: `link-e${e.id}-l${e.locationId}`,
+        target: 'equipment-' + e.id,
+        source: 'location-' + e.locationId,
+        stroke: '#2196f3',
+      }
+    }));
     this.links = relationships;
   }
 }
