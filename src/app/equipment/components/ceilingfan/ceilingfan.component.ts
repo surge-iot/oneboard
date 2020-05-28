0ba0ba085c67eb0545a947d9daf66a8181ffe1a3
Shinjan Mitra <shinjanxp@gmail.com>
2020-05-28 14:01:50 +0530

    feat: Adds conditional command display

--- src/app/equipment/components/ceilingfan/ceilingfan.component.ts
+++ src/app/equipment/components/ceilingfan/ceilingfan.component.ts
@@ -8,15 +8,16 @@ import { Equipment } from '../../equipment.service';
 })
 export class CeilingfanComponent implements OnInit {
 
-  constructor() {
-    this.switch = false;
-    this.speed = 0;
-   }
+  constructor() { }
   @Input() equipment: Equipment;
-  speed: number;
-  switch: boolean;
+  speed: number = 0;
+  switch: boolean = false;
+  speedCommandExists: boolean = false;
+  switchCommandExists: boolean = false;
   ngOnInit(): void {
     try {
+      this.switchCommandExists = this.equipment.points.filter(p => p.classId === 'COMMAND.SWITCH').length != 0;
+      this.speedCommandExists = this.equipment.points.filter(p => p.classId === 'COMMAND.SPEED').length != 0;
       this.switch = this.equipment.points.filter((point) => point.classId === 'SETPOINT.SWITCH')[0].meta.switch;
       this.speed = this.equipment.points.filter((point) => point.classId === 'SETPOINT.SPEED')[0].meta.speed;
     }
