5903d18847f4f85f87c8a1fec604039b2dffc580
Shinjan Mitra <shinjanxp@gmail.com>
2020-06-05 07:27:43 +0530

    refactor: Renames project to oneboard-ui

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('oneboard-web app is running!');
+    expect(page.getTitleText()).toEqual('oneboard-ui app is running!');
   });
 
   afterEach(async () => {
